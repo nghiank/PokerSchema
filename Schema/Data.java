f140d87c7b0500408dbdef861a2d67b1f2acb691
Nghia Nguyen <nghiank@hotmail.com>
2018-01-31 20:38:05 -0800

    Add PlayerInfo and Room

--- Schema/Data.java
+++ Schema/Data.java
@@ -8,8 +8,9 @@ public final class Data {
   public static final byte CredentialToken = 1;
   public static final byte ReconnectKey = 2;
   public static final byte JoinRoomCommand = 3;
+  public static final byte PlayerInfo = 4;
 
-  public static final String[] names = { "NONE", "CredentialToken", "ReconnectKey", "JoinRoomCommand", };
+  public static final String[] names = { "NONE", "CredentialToken", "ReconnectKey", "JoinRoomCommand", "PlayerInfo", };
 
   public static String name(int e) { return names[e]; }
 }
--- gen.sh
+++ gen.sh
@@ -1,4 +1,8 @@
 rm -rf Schema/
 flatc --csharp schema.fbs
 flatc --java schema.fbs
+rm -rf ../GameServer/src/main/java/schema
+find schema/ -name "*.java" | cpio -pdm ../GameServer/src/main/java
+rm -rf ../Poker/Assets/Scripts/Schema
+find schema/ -name "*.cs" | cpio -pdm ../Poker/Assets/Scripts
 git add .
