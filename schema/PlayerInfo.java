112de2dbdd589fff43f22c06dff253453f3c9515
Nghia Nguyen <nghiank@hotmail.com>
2018-01-31 20:59:32 -0800

    Update playerinfo

--- schema/PlayerInfo.java
+++ schema/PlayerInfo.java
@@ -14,18 +14,23 @@ public final class PlayerInfo extends Table {
   public void __init(int _i, ByteBuffer _bb) { bb_pos = _i; bb = _bb; }
   public PlayerInfo __assign(int _i, ByteBuffer _bb) { __init(_i, _bb); return this; }
 
-  public String name() { int o = __offset(4); return o != 0 ? __string(o + bb_pos) : null; }
-  public ByteBuffer nameAsByteBuffer() { return __vector_as_bytebuffer(4, 1); }
+  public String userId() { int o = __offset(4); return o != 0 ? __string(o + bb_pos) : null; }
+  public ByteBuffer userIdAsByteBuffer() { return __vector_as_bytebuffer(4, 1); }
+  public String name() { int o = __offset(6); return o != 0 ? __string(o + bb_pos) : null; }
+  public ByteBuffer nameAsByteBuffer() { return __vector_as_bytebuffer(6, 1); }
 
   public static int createPlayerInfo(FlatBufferBuilder builder,
+      int userIdOffset,
       int nameOffset) {
-    builder.startObject(1);
+    builder.startObject(2);
     PlayerInfo.addName(builder, nameOffset);
+    PlayerInfo.addUserId(builder, userIdOffset);
     return PlayerInfo.endPlayerInfo(builder);
   }
 
-  public static void startPlayerInfo(FlatBufferBuilder builder) { builder.startObject(1); }
-  public static void addName(FlatBufferBuilder builder, int nameOffset) { builder.addOffset(0, nameOffset, 0); }
+  public static void startPlayerInfo(FlatBufferBuilder builder) { builder.startObject(2); }
+  public static void addUserId(FlatBufferBuilder builder, int userIdOffset) { builder.addOffset(0, userIdOffset, 0); }
+  public static void addName(FlatBufferBuilder builder, int nameOffset) { builder.addOffset(1, nameOffset, 0); }
   public static int endPlayerInfo(FlatBufferBuilder builder) {
     int o = builder.endObject();
     return o;
